d733d264c87d8944fddfaf762ce1108a2248e7c7
kiwier <roczhmg@163.com>
2021-04-22 12:03:17 +0800

    新增api

--- main.go
+++ main.go
@@ -11,33 +11,6 @@ func main() {
 	database.Start()
 	model.Migrate()
 
-	//database.DB.
-	//i:= model.IDCard{Num: 123456}
-	//s:= model.Student{
-	//	StudentName: "kiwi",
-	//	IDCard: i,
-	//}
-
-	//t:= model.Teacher{
-	//	TeacherName: "腾跃文",
-	//	Students: []model.Student{s},
-	//}
-	//
-	//c:= model.Class{
-	//	ClassName: "实验一班",
-	//	Student: []model.Student{s},
-	//}
-	//if err := database.DB.Create(&c).Error; err != nil {
-	//	panic(err)
-	//}
-
-	//if err := database.DB.Create(&s).Error; err != nil {
-	//	panic(err)
-	//}
-	//if err := database.DB.Create(&t).Error; err != nil {
-	//	panic(err)
-	//}
-
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -97,5 +70,18 @@ func main() {
 			"message": "ok",
 		})
 	})
+
+
+	r.GET("/teacher", func(context *gin.Context) {
+		teacherName := context.DefaultQuery("name", "Guest") // 此方法可以设置默认值
+		var data []model.Teacher
+		database.DB.Select([]string{"TeacherName","TeachName","CreatedAt","ID","UpdatedAt"}).Where("teacher_name = ?", teacherName).First(&data)
+		context.JSON(200, gin.H{
+			"code":200,
+			"message": "ok",
+			"data":data,
+		})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
